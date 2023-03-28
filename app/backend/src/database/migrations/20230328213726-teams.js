0f655990139e4b0f5272bae5e750c234493d27e6
welitonlimaa <welitonlima159@gmail.com>
2023-03-28 19:33:17 -0300

    req 01 fluxo 01

---
+++ app/backend/src/database/migrations/20230328213726-teams.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      team_name: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,37 @@
+import { Model, DataTypes, CreationOptional } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: CreationOptional<number>;;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: new DataTypes.STRING(128),
+    allowNull: false,
+  },
+}, {
+  tableName: 'teams',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
