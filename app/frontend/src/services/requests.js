3e228ac1d41f70e48e26b7cbb5e258940f713a62
welitonlimaa <welitonlima159@gmail.com>
2023-04-28 11:34:58 -0300

    fix: endpoint

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://mfc-service.up.railway.app",
+  baseURL: "https://mfc-service.up.railway.app/",
 });
 
 export const setToken = (token) => {
